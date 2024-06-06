6c8281c093fe56bd07c5b34336239020996e6f46
Camila Pozas <89259499+camipozas@users.noreply.github.com>
2024-06-06 01:56:37 -0400

    chore: add OpenAI request helper function

---
+++ src/helpers/openai-request.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import config from '../config/environment';
+import { OpenAIRequest } from './openai.types';
+
+/**
+ * Sends a request to the OpenAI API to generate a response to the user's message.
+ * @param userMessage The message from the user.
+ * @returns The response from the OpenAI API.
+ * @throws An error if the request fails.
+ */
+export async function openAIRequest(userMessage: string): Promise<any> {
+  const openAIRequest: OpenAIRequest = {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: userMessage },
+    ],
+    temperature: 0.7,
+  };
+
+  try {
+    const response = await axios.post(
+      'https://api.openai.com/v1/chat/completions',
+      openAIRequest,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${config.openai.apiKey}`,
+        },
+      }
+    );
+
+    return response.data.choices[0].message.content;
+  } catch (error) {
+    console.error(error);
+    return new Error('An error occurred while processing your request.');
+  }
+}
---
+++ test/helpers/openai-request.test.ts
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import { openAIRequest } from '../../src/helpers/openai-request';
+
+const expectedResponse = {
+  "id": "chatcmpl-9WzgIJJSZGhLfxJtoc3OqAruPZXPS",
+  "object": "chat.completion",
+  "created": 1717650626,
+  "model": "gpt-3.5-turbo-0125",
+  "choices": [
+    {
+      "index": 0,
+      "message": {
+        "role": "assistant",
+        "content": "Hello! How can I assist you today?"
+      },
+      "logprobs": null,
+      "finish_reason": "stop"
+    }
+  ],
+  "usage": {
+    "prompt_tokens": 19,
+    "completion_tokens": 9,
+    "total_tokens": 28
+  },
+  "system_fingerprint": null
+};
+
+jest.mock('axios');
+
+describe('HELPER: openAIRequest', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+
+  it('[SUCCESS] should return a mocked response', async () => {
+    axios.post = jest.fn().mockResolvedValue({ data: expectedResponse });
+
+    const userMessage = 'Hello!';
+    const response = await openAIRequest(userMessage);
+
+    expect(response).toBe(expectedResponse.choices[0].message.content);
+  });
+
+  it('[ERROR] should return an error message if `https://api.openai.com/v1/chat/completions` fails', async () => {
+    axios.post = jest.fn().mockRejectedValue(new Error('Failed to fetch data'));
+
+    const userMessage = 'Hello!';
+    try {
+      await openAIRequest(userMessage);
+    } catch (error) {
+      expect(error).toBeDefined();
+      expect(error).toEqual(new Error('An error occurred while processing your request.'));
+    }
+  });
+});
